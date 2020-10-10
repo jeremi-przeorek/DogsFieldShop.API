217389275a4d07eee1d016b5c28a32086bca8d64
jeremi-przeorek <57287376+jeremi-przeorek@users.noreply.github.com>
2020-10-10 22:52:22 +0200

    Created ApplicationServicesExtensions.

--- DogsFieldShop.API/Extensions/ApplicationServicesExtensions.cs
+++ DogsFieldShop.API/Extensions/ApplicationServicesExtensions.cs
@@ -1,11 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using AutoMapper;
+using DogsFieldShop.API.Errors;
+using DogsFieldShop.API.Helpers;
+using DogsFieldShop.Core.Interfaces;
+using DogsFieldShop.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace DogsFieldShop.API.Extensions
 {
-    public class ApplicationServicesExtensions
+    public static class ApplicationServicesExtensions
     {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddAutoMapper(typeof(MappingProfiles));
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    var errors = actionContext.ModelState
+                        .Where(e => e.Value.Errors.Any())
+                        .SelectMany(x => x.Value.Errors)
+                        .Select(x => x.ErrorMessage)
+                        .ToArray();
+
+                    var errorResponse = new ApiValidationErrorResponse
+                    {
+                        Errors = errors,
+                    };
+
+                    return new BadRequestObjectResult(errorResponse);
+                };
+            });
+
+            return services;
+        }
     }
 }
