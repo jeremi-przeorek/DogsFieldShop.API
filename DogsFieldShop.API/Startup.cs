f514ae043679022dd3e48391fa63f92e9205abe5
jeremi-przeorek <57287376+jeremi-przeorek@users.noreply.github.com>
2020-10-10 21:43:54 +0200

    Added SW4GGERRRRR.

--- DogsFieldShop.API/Startup.cs
+++ DogsFieldShop.API/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System.Linq;
 
 namespace DogsFieldShop.Infrastructure
@@ -51,6 +52,15 @@ namespace DogsFieldShop.Infrastructure
                     return new BadRequestObjectResult(errorResponse);
                 };
             });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "DogsFieldShop API",
+                    Version = "v1",
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -67,6 +77,12 @@ namespace DogsFieldShop.Infrastructure
             app.UseStaticFiles();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "DogsFieldShop API v1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
